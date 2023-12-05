839336baf925da1600ee844d136a54cef4b3c103
TheNetRunner <smith.rc.david@icloud.com>
2023-12-05 10:40:57 +0000

    finished character list page

---
+++ src/app/shadow-run-characters/data/fakeCharacters.data.ts
@@ -0,0 +1,4 @@
+export const fakeCharacters = [
+    { id: "123-456", name: "David", metaType: "human"}, 
+    { id: "789-101112", name: "Ben", metaType: "troll" }
+]
--- src/app/shadow-run-characters/list-view/list-view.component.ts
+++ src/app/shadow-run-characters/list-view/list-view.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { v4 as uuidv4 } from 'uuid';
+
+import { fakeCharacters } from '../data/fakeCharacters.data';
 
 @Component({
   selector: 'app-list-view',
@@ -6,5 +9,33 @@ import { Component } from '@angular/core';
   styleUrl: './list-view.component.css'
 })
 export class ListViewComponent {
+    characters: any[] = fakeCharacters;
+
+    copyCharacter(characterIndex: number): void {
+        if (this.characters.length < 25) {
+            let characterCopy = this.characters[characterIndex];
+            characterCopy.id = uuidv4()
+            characterCopy.name = `${characterCopy.name} copy`
+
+            this.characters.push(characterCopy);
+        }
+    }
+
+    deleteCharacter(characterIndex: number): void {
+        this.characters.splice(characterIndex, 1);
+    }
+
+    getRandomBackgroundImage(): any {
+        const choices = ["one", "two", "three", "four"];
+        const randomIndex = Math.floor(Math.random() * choices.length);
+        const choice = choices[randomIndex];
+        console.log(choice);
+
+        return { "background-image": `url(../../../assets/imgs/shadow-run/character_background_${choice}.png)` }
+    }
+
+    get totalCharacters(): number {
+        return this.characters.length;
+    }
 
 }
